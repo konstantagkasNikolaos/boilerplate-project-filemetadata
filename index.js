8404a9cf09053f40be38d7e8fb9f2aa0feda409f
konstantagkas <konstantagkas.nikolaos@gmail.com>
2022-12-06 10:53:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+const upload = multer({ dest: "uploads/" });
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+
+  if (!req.file) return res.send("No File Found to Upload");
+
+  res.json({ name: originalname, type: mimetype, size: size });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
